b9ab815d2c731c08a0840a190c3b77548d853245
priyanshu <robin01june@gmail.com>
2023-05-31 22:42:13 +0530

    insane hardwork

---
+++ js/index.js
@@ -0,0 +1,658 @@
+
+window.requestAnimFrame = function () {
+  return window.requestAnimationFrame ||
+  window.webkitRequestAnimationFrame ||
+  window.mozRequestAnimationFrame ||
+  function (callback) {
+    window.setTimeout(callback, 1000 / 60);
+  };
+}();
+
+// now we will setup our basic variables for the demo
+var canvas = document.getElementById('canvas'),
+ctx = canvas.getContext('2d'),
+// full screen dimensions
+cw = window.innerWidth,
+ch = window.innerHeight,
+// firework collection
+fireworks = [],
+// particle collection
+particles = [],
+// starting hue
+hue = 120,
+// when launching fireworks with a click, too many get launched at once without a limiter, one launch per 5 loop ticks
+limiterTotal = 5,
+limiterTick = 0,
+// this will time the auto launches of fireworks, one launch per 80 loop ticks
+timerTotal = 80,
+timerTick = 0,
+mousedown = false,
+// mouse x coordinate,
+mx,
+// mouse y coordinate
+my;
+
+// set canvas dimensions
+canvas.width = cw;
+canvas.height = ch;
+
+// now we are going to setup our function placeholders for the entire demo
+
+// get a random number within a range
+function random(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+// calculate the distance between two points
+function calculateDistance(p1x, p1y, p2x, p2y) {
+  var xDistance = p1x - p2x,
+  yDistance = p1y - p2y;
+  return Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2));
+}
+
+// create firework
+function Firework(sx, sy, tx, ty) {
+  // actual coordinates
+  this.x = sx;
+  this.y = sy;
+  // starting coordinates
+  this.sx = sx;
+  this.sy = sy;
+  // target coordinates
+  this.tx = tx;
+  this.ty = ty;
+  // distance from starting point to target
+  this.distanceToTarget = calculateDistance(sx, sy, tx, ty);
+  this.distanceTraveled = 0;
+  // track the past coordinates of each firework to create a trail effect, increase the coordinate count to create more prominent trails
+  this.coordinates = [];
+  this.coordinateCount = 3;
+  // populate initial coordinate collection with the current coordinates
+  while (this.coordinateCount--) {
+    this.coordinates.push([this.x, this.y]);
+  }
+  this.angle = Math.atan2(ty - sy, tx - sx);
+  this.speed = 2;
+  this.acceleration = 1.05;
+  this.brightness = random(50, 70);
+  // circle target indicator radius
+  this.targetRadius = 1;
+}
+
+// update firework
+Firework.prototype.update = function (index) {
+  // remove last item in coordinates array
+  this.coordinates.pop();
+  // add current coordinates to the start of the array
+  this.coordinates.unshift([this.x, this.y]);
+
+  // cycle the circle target indicator radius
+  if (this.targetRadius < 8) {
+    this.targetRadius += 0.3;
+  } else {
+    this.targetRadius = 1;
+  }
+
+  // speed up the firework
+  this.speed *= this.acceleration;
+
+  // get the current velocities based on angle and speed
+  var vx = Math.cos(this.angle) * this.speed,
+  vy = Math.sin(this.angle) * this.speed;
+  // how far will the firework have traveled with velocities applied?
+  this.distanceTraveled = calculateDistance(this.sx, this.sy, this.x + vx, this.y + vy);
+
+  // if the distance traveled, including velocities, is greater than the initial distance to the target, then the target has been reached
+  if (this.distanceTraveled >= this.distanceToTarget) {
+    createParticles(this.tx, this.ty);
+    // remove the firework, use the index passed into the update function to determine which to remove
+    fireworks.splice(index, 1);
+  } else {
+    // target not reached, keep traveling
+    this.x += vx;
+    this.y += vy;
+  }
+};
+
+// draw firework
+Firework.prototype.draw = function () {
+  ctx.beginPath();
+  // move to the last tracked coordinate in the set, then draw a line to the current x and y
+  ctx.moveTo(this.coordinates[this.coordinates.length - 1][0], this.coordinates[this.coordinates.length - 1][1]);
+  ctx.lineTo(this.x, this.y);
+  ctx.strokeStyle = 'hsl(' + hue + ', 100%, ' + this.brightness + '%)';
+  ctx.stroke();
+
+  ctx.beginPath();
+  // draw the target for this firework with a pulsing circle
+  ctx.arc(this.tx, this.ty, this.targetRadius, 0, Math.PI * 2);
+  ctx.stroke();
+};
+
+// create particle
+function Particle(x, y) {
+  this.x = x;
+  this.y = y;
+  // track the past coordinates of each particle to create a trail effect, increase the coordinate count to create more prominent trails
+  this.coordinates = [];
+  this.coordinateCount = 5;
+  while (this.coordinateCount--) {
+    this.coordinates.push([this.x, this.y]);
+  }
+  // set a random angle in all possible directions, in radians
+  this.angle = random(0, Math.PI * 2);
+  this.speed = random(1, 10);
+  // friction will slow the particle down
+  this.friction = 0.95;
+  // gravity will be applied and pull the particle down
+  this.gravity = 1;
+  // set the hue to a random number +-20 of the overall hue variable
+  this.hue = random(hue - 20, hue + 20);
+  this.brightness = random(50, 80);
+  this.alpha = 1;
+  // set how fast the particle fades out
+  this.decay = random(0.015, 0.03);
+}
+
+// update particle
+Particle.prototype.update = function (index) {
+  // remove last item in coordinates array
+  this.coordinates.pop();
+  // add current coordinates to the start of the array
+  this.coordinates.unshift([this.x, this.y]);
+  // slow down the particle
+  this.speed *= this.friction;
+  // apply velocity
+  this.x += Math.cos(this.angle) * this.speed;
+  this.y += Math.sin(this.angle) * this.speed + this.gravity;
+  // fade out the particle
+  this.alpha -= this.decay;
+
+  // remove the particle once the alpha is low enough, based on the passed in index
+  if (this.alpha <= this.decay) {
+    particles.splice(index, 1);
+  }
+};
+
+// draw particle
+Particle.prototype.draw = function () {
+  ctx.beginPath();
+  // move to the last tracked coordinates in the set, then draw a line to the current x and y
+  ctx.moveTo(this.coordinates[this.coordinates.length - 1][0], this.coordinates[this.coordinates.length - 1][1]);
+  ctx.lineTo(this.x, this.y);
+  ctx.strokeStyle = 'hsla(' + this.hue + ', 100%, ' + this.brightness + '%, ' + this.alpha + ')';
+  ctx.stroke();
+};
+
+// create particle group/explosion
+function createParticles(x, y) {
+  // increase the particle count for a bigger explosion, beware of the canvas performance hit with the increased particles though
+  var particleCount = 30;
+  while (particleCount--) {
+    particles.push(new Particle(x, y));
+  }
+}
+
+// main demo loop
+function loop() {
+  // this function will run endlessly with requestAnimationFrame
+  requestAnimFrame(loop);
+
+  // increase the hue to get different colored fireworks over time
+  hue += 0.5;
+
+  // normally, clearRect() would be used to clear the canvas
+  // we want to create a trailing effect though
+  // setting the composite operation to destination-out will allow us to clear the canvas at a specific opacity, rather than wiping it entirely
+  ctx.globalCompositeOperation = 'destination-out';
+  // decrease the alpha property to create more prominent trails
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.5)';
+  ctx.fillRect(0, 0, cw, ch);
+  // change the composite operation back to our main mode
+  // lighter creates bright highlight points as the fireworks and particles overlap each other
+  ctx.globalCompositeOperation = 'lighter';
+
+  // loop over each firework, draw it, update it
+  var i = fireworks.length;
+  while (i--) {
+    fireworks[i].draw();
+    fireworks[i].update(i);
+  }
+
+  // loop over each particle, draw it, update it
+  var i = particles.length;
+  while (i--) {
+    particles[i].draw();
+    particles[i].update(i);
+  }
+
+  // launch fireworks automatically to random coordinates, when the mouse isn't down
+  if (timerTick >= timerTotal) {
+    if (!mousedown) {
+      // start the firework at the bottom middle of the screen, then set the random target coordinates, the random y coordinates will be set within the range of the top half of the screen
+      fireworks.push(new Firework(cw / 2, ch, random(0, cw), random(0, ch / 2)));
+      timerTick = 0;
+    }
+  } else {
+    timerTick++;
+  }
+
+  // limit the rate at which fireworks get launched when mouse is down
+  if (limiterTick >= limiterTotal) {
+    if (mousedown) {
+      // start the firework at the bottom middle of the screen, then set the current mouse coordinates as the target
+      fireworks.push(new Firework(cw / 2, ch, mx, my));
+      limiterTick = 0;
+    }
+  } else {
+    limiterTick++;
+  }
+}
+
+window.onload = function () {
+  var merrywrap = document.getElementById("merrywrap");
+  var box = merrywrap.getElementsByClassName("giftbox")[0];
+  var step = 1;
+  var stepMinutes = [2000, 2000, 1000, 1000];
+  function init() {
+    box.addEventListener("click", openBox, false);
+  }
+  function stepClass(step) {
+    merrywrap.className = 'merrywrap';
+    merrywrap.className = 'merrywrap step-' + step;
+  }
+  function openBox() {
+    if (step === 1) {
+      box.removeEventListener("click", openBox, false);
+    }
+    stepClass(step);
+    if (step === 3) {
+    }
+    if (step === 4) {
+      reveal();
+      return;
+    }
+    setTimeout(openBox, stepMinutes[step - 1]);
+    step++;
+  }
+
+  init();
+
+};
+
+function reveal() {
+  document.querySelector('.merrywrap').style.backgroundColor = 'transparent';
+
+  loop();
+
+  var w, h;
+  if (window.innerWidth >= 1000) {
+    w = 295;h = 185;
+  } else
+  {
+    w = 255;h = 155;
+  }
+
+
+  var ifrm = document.createElement("iframe");
+  ifrm.setAttribute("src", "https://www.youtube.com/embed/jkRV-V90cmc");
+// Rest of the code...
+
+  //ifrm.style.width = `${w}px`;
+  //ifrm.style.height = `${h}px`;
+  ifrm.style.border = 'none';
+
+  document.querySelector('#video').appendChild(ifrm);
+  function playVideo() {
+    ifrm.contentWindow.postMessage('{"event":"command","func":"playVideo","args":""}', '*');
+  }
+  
+  // Add an event listener to the iframe to detect when it has loaded
+  ifrm.addEventListener('load', function() {
+    playVideo();
+  });
+  revealcont();
+}
+
+function revealcont(){
+  var merry = document.querySelector('.merrywrap');
+  merry.style.backgroundImage = 'none';
+  var cont = document.querySelector('.container');
+  console.log(cont);
+  // var hat = document.getElementById('hat');
+  // console.log(hat);
+  // hat.style.display = "none";
+  setTimeout(()=>{
+    
+
+
+// Import the data to customize and insert them into page
+const fetchData = () => {
+
+  console.log("priyanshu");
+  fetch("js/customize.json")
+    .then(data => data.json())
+    .then(data => {
+      dataArr = Object.keys(data);
+      console.log(dataArr);
+      dataArr.map(customData => {
+          console.log(customData);
+        if (data[customData] !== "") {
+          if (customData === "imagePath") {
+            document
+              .querySelector(`[data-node-name*="${customData}"]`)
+              .setAttribute("src", data[customData]);
+          } else {
+            document.querySelector(`[data-node-name*="${customData}"]`).innerText = data[customData];
+          }
+        }
+
+        // Check if the iteration is over
+        // Run amimation if so
+        if ( dataArr.length === dataArr.indexOf(customData) + 1 ) {
+          animationTimeline();
+        } 
+      });
+    });
+};
+
+
+
+// Animation Timeline
+const animationTimeline = () => {
+  // Spit chars that needs to be animated individually
+  const textBoxChars = document.getElementsByClassName("hbd-chatbox")[0];
+  const hbd = document.getElementsByClassName("wish-hbd")[0];
+
+  textBoxChars.innerHTML = `<span>${textBoxChars.innerHTML
+    .split("")
+    .join("</span><span>")}</span`;
+
+  hbd.innerHTML = `<span>${hbd.innerHTML
+    .split("")
+    .join("</span><span>")}</span`;
+
+  const ideaTextTrans = {
+    opacity: 0,
+    y: -20,
+    rotationX: 5,
+    skewX: "15deg"
+  };
+
+  const ideaTextTransLeave = {
+    opacity: 0,
+    y: 20,
+    rotationY: 5,
+    skewX: "-15deg"
+  };
+
+  const tl = new TimelineMax();
+
+  tl
+    .to(".container", 0.1, {
+      visibility: "visible"
+    })
+    .from(".one", 0.7, {
+      opacity: 0,
+      y: 10
+    })
+    .from(".two", 0.4, {
+      opacity: 0,
+      y: 10
+    })
+    .to(
+      ".one",
+      0.7,
+      {
+        opacity: 0,
+        y: 10
+      },
+      "+=2.5"
+    )
+    .to(
+      ".two",
+      0.7,
+      {
+        opacity: 0,
+        y: 10
+      },
+      "-=1"
+    )
+    .from(".three", 0.7, {
+      opacity: 0,
+      y: 10
+      // scale: 0.7
+    })
+    .to(
+      ".three",
+      0.7,
+      {
+        opacity: 0,
+        y: 10
+      },
+      "+=2"
+    )
+    .from(".four", 0.7, {
+      scale: 0.2,
+      opacity: 0
+    })
+    .from(".fake-btn", 0.3, {
+      scale: 0.2,
+      opacity: 0
+    })
+    .staggerTo(
+      ".hbd-chatbox span",
+      0.5,
+      {
+        visibility: "visible"
+      },
+      0.05
+    )
+    .to(".fake-btn", 0.1, {
+      backgroundColor: "rgb(127, 206, 248)"
+    })
+    .to(
+      ".four",
+      0.5,
+      {
+        scale: 0.2,
+        opacity: 0,
+        y: -150
+      },
+      "+=0.7"
+    )
+    .from(".idea-1", 0.7, ideaTextTrans)
+    .to(".idea-1", 0.7, ideaTextTransLeave, "+=1.5")
+    .from(".idea-2", 0.7, ideaTextTrans)
+    .to(".idea-2", 0.7, ideaTextTransLeave, "+=1.5")
+    .from(".idea-3", 0.7, ideaTextTrans)
+    .to(".idea-3 strong", 0.5, {
+      scale: 1.2,
+      x: 10,
+      backgroundColor: "rgb(21, 161, 237)",
+      color: "#fff"
+    })
+    .to(".idea-3", 0.7, ideaTextTransLeave, "+=1.5")
+    .from(".idea-4", 0.7, ideaTextTrans)
+    .to(".idea-4", 0.7, ideaTextTransLeave, "+=1.5")
+    .from(
+      ".idea-5",
+      0.7,
+      {
+        rotationX: 15,
+        rotationZ: -10,
+        skewY: "-5deg",
+        y: 50,
+        z: 10,
+        opacity: 0
+      },
+      "+=0.5"
+    )
+    .to(
+      ".idea-5 .smiley",
+      0.7,
+      {
+        rotation: 90,
+        x: 8
+      },
+      "+=0.4"
+    )
+    .to(
+      ".idea-5",
+      0.7,
+      {
+        scale: 0.2,
+        opacity: 0
+      },
+      "+=2"
+    )
+    .staggerFrom(
+      ".idea-6 span",
+      0.8,
+      {
+        scale: 3,
+        opacity: 0,
+        rotation: 15,
+        ease: Expo.easeOut
+      },
+      0.2
+    )
+    .staggerTo(
+      ".idea-6 span",
+      0.8,
+      {
+        scale: 3,
+        opacity: 0,
+        rotation: -15,
+        ease: Expo.easeOut
+      },
+      0.2,
+      "+=1"
+    )
+    .staggerFromTo(
+      ".baloons img",
+      2.5,
+      {
+        opacity: 0.9,
+        y: 1400
+      },
+      {
+        opacity: 1,
+        y: -1000
+      },
+      0.2
+    )
+    .from(
+      ".lydia-dp",
+      0.5,
+      {
+        scale: 3.5,
+        opacity: 0,
+        x: 25,
+        y: -25,
+        rotationZ: -45
+      },
+      "-=2"
+    )
+    .from(".hat", 0.5, {
+      x: -100,
+      y: 350,
+      rotation: -180,
+      opacity: 0
+    })
+    .staggerFrom(
+      ".wish-hbd span",
+      0.7,
+      {
+        opacity: 0,
+        y: -50,
+        // scale: 0.3,
+        rotation: 150,
+        skewX: "30deg",
+        ease: Elastic.easeOut.config(1, 0.5)
+      },
+      0.1
+    )
+    .staggerFromTo(
+      ".wish-hbd span",
+      0.7,
+      {
+        scale: 1.4,
+        rotationY: 150
+      },
+      {
+        scale: 1,
+        rotationY: 0,
+        color: "#ff69b4",
+        ease: Expo.easeOut
+      },
+      0.1,
+      "party"
+    )
+    .from(
+      ".wish h5",
+      0.5,
+      {
+        opacity: 0,
+        y: 10,
+        skewX: "-15deg"
+      },
+      "party"
+    )
+    .staggerTo(
+      ".eight svg",
+      1.5,
+      {
+        visibility: "visible",
+        opacity: 0,
+        scale: 80,
+        repeat: 3,
+        repeatDelay: 1.4
+      },
+      0.3
+    )
+    .to(".six", 0.5, {
+      opacity: 0,
+      y: 30,
+      zIndex: "-1"
+    })
+    .staggerFrom(".nine p", 1, ideaTextTrans, 1.2)
+    .to(
+      ".last-smile",
+      0.5,
+      {
+        rotation: 90
+      },
+      "+=1"
+    );
+
+
+
+    
+
+  // tl.seek("currentStep");
+  // tl.timeScale(2);
+
+  // Restart Animation on click
+  const replyBtn = document.getElementById("replay");
+  replyBtn.addEventListener("click", () => {
+    tl.restart();
+  });
+};
+
+// Run fetch and animation in sequence
+fetchData();
+
+//   addEventListener("DOMContentLoaded",()=>{
+//     var audio1 = new Audio('perfect.mp3');
+//     audio1.play();
+//   });
+setTimeout(()=>{
+  cont.style.display = "block";
+},500);
+
+  },2500);
+
+}
+
+
